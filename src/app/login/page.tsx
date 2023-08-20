12162aed631ee19fe67d2733161d01d2df6b1400
Stanisław Synowiec <stasyn1410@gmail.com>
2023-08-20 18:44:14 +0200

    fix: login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,14 +11,15 @@ type Inputs = {
 };
 
 const loginSchema = yup.object({
-	login: yup.string().required(),
-	password: yup.string().required(),
+	login: yup.string().required('Login is required'),
+	password: yup.string().required('Password is required'),
 });
 
 const LoginPage = () => {
 	const {
 		register,
 		handleSubmit,
+		setError,
 		formState: { errors },
 	} = useForm<Inputs>({
 		resolver: yupResolver(loginSchema),
@@ -35,34 +36,20 @@ const LoginPage = () => {
 				credentials: 'include',
 			},
 		);
-		console.log(response);
-		fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
-			method: 'GET',
-			credentials: 'same-origin',
-		})
-			.then(response => response.json())
-			.then(users => console.log(users));
+
+		if (response.ok) {
+			console.log('logged');
+		} else {
+			setError('root', { message: 'Invalid login or password' });
+		}
 	};
 
 	return (
-		/* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-		// <form onSubmit={handleSubmit(onSubmit)}>
-		// 	{/* register your input into the hook by invoking the "register" function */}
-		// 	<input {...register('login', { required: true })} />
-		// 	{errors.login && <span>This field is required</span>}
-		//
-		// 	{/* include validation with required or other standard HTML validation rules */}
-		// 	<input {...register('password', { required: true })} />
-		// 	{/* errors will return when field validation fails  */}
-		// 	{errors.password && <span>This field is required</span>}
-		//
-		// 	<input type="submit" />
-		// </form>
 		<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 			<div className="sm:mx-auto sm:w-full sm:max-w-sm">
 				<Image
 					className="mx-auto h-10 w-auto"
-					src=""
+					src="/favicon.ico"
 					alt="Your Company"
 					width={40}
 					height={40}
@@ -96,6 +83,7 @@ const LoginPage = () => {
 								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 							/>
 						</div>
+						{errors.login && <span>{errors.login.message}</span>}
 					</div>
 
 					<div>
@@ -125,8 +113,9 @@ const LoginPage = () => {
 								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 							/>
 						</div>
+						{errors.password && <span>{errors.password.message}</span>}
 					</div>
-
+					{errors.root && <span>{errors.root.message}</span>}
 					<div>
 						<button
 							type="submit"
